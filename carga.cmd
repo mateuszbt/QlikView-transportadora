77b86fae56b9bf8f37bf57b63dbfb0a8896a559a
Mateus Zabote <mateus.zabotedasilva3@gmail.com>
2018-11-07 07:56:37 -0200

    commit de transportadora

---
+++ carga.cmd
@@ -0,0 +1,5 @@
+
+"C:\Program Files\QlikView\Qv.exe" /R "C:\Users\Mateus\Desktop\udemy\curso de qlikview\Projeto 2 - Transportadora\Extracao\extracao.qvw"
+"C:\Program Files\QlikView\Qv.exe" /R "C:\Users\Mateus\Desktop\udemy\curso de qlikview\Projeto 2 - Transportadora\Transformacao\transformacao.qvw"
+"C:\Program Files\QlikView\Qv.exe" /R "C:\Users\Mateus\Desktop\udemy\curso de qlikview\Projeto 2 - Transportadora\Visualizacao\dashboard.qvw"
+Exit;
